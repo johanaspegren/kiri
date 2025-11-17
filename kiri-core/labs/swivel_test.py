3de26bfdef7508de106ee001a766058a0e994118
johanaspegren <johan.aspegren@gmail.com>
2025-11-17 22:30:24 +0100

    updated old swivel test

--- kiri-core/labs/swivel_test.py
+++ kiri-core/labs/swivel_test.py
@@ -5,8 +5,29 @@ from hardware.swivel import SwivelController
 async def run():
     with SwivelController().open() as sw:
         print("KIRI waking...")
-        sw.center()
-        print("Done.")
+        sw.pan(-30)
+        await asyncio.sleep(0.4)
+        sw.pan(30)
+        await asyncio.sleep(0.4)
+
+        print("KIRI waking...")
+        sw.tilt(-30)
+        await asyncio.sleep(0.4)
+        sw.tilt(30)
+        await asyncio.sleep(0.4)
+
+        print("KIRI waking...")
+        sw.set(90, 90)
+        await asyncio.sleep(0.4)
+        sw.set(60, 60)
+        await asyncio.sleep(0.4)
+        sw.set(120, 120)
+        await asyncio.sleep(0.4)
+        sw.set(90, 90)
+        await asyncio.sleep(0.4)
+
+
+    print("Done.")
 
 if __name__ == "__main__":
     asyncio.run(run())
